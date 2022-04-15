d85f5a19e3159bf0f67e0a46a9430ebf461067f2
Akanksha Dinesh Jagdale <79737871+akankshajagdale@users.noreply.github.com>
2022-04-15 14:29:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 # Creating the Titles and Image
 st.title("Predict Insurance Bill")
-st.header("Calculating the Insurance Bill based on a person's attributes")
-
 
 def load_data():
     df = pd.DataFrame({'sex': ['Male','Female'],
@@ -26,12 +24,12 @@ def load_data():
 df1 = load_data()
 
 # Take the users input
-
-sex = st.selectbox("Select gender", df['sex'].unique())
-smoker = st.selectbox("Are you a smoker", df['smoker'].unique())
-region = st.selectbox("Select your region?", df1['region'].unique())
-age = st.slider("What is your age?", 18, 100)
-bmi = st.slider("What is your bmi?", 10, 60)
+name = st.text_input("What is Firstname?")
+sex = st.radio("Select Gender",("male", "female"))
+smoker = st.selectbox("Are you a Smoker", df['smoker'].unique())
+region = st.selectbox("Select your Region?", df1['region'].unique())
+age = st.slider("What is your Age?", 18, 100)
+bmi = st.slider("What is your BMI?", 10, 60)
 children = st.slider("Number of children", 0, 10)
 
 # converting text input to numeric to get back predictions from backend model.
@@ -65,7 +63,5 @@ final_features = [np.array(int_features)]
 
 if st.button('Predict'):           # when the submit button is pressed
     prediction =  model.predict(final_features)
-    st.balloons()
-    st.success(f'Your insurance bill would be: ${round(prediction[0],2)}')
-    
-
+#     st.success(f'Your insurance bill would be: ${round(prediction[0],0)}')
+    st.success('{}, your insurance bill would be ${}'.format(name, round(prediction[0],0) ))
